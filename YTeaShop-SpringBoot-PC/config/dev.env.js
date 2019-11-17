eeff468d1df2e2c87087fb8ece2ca703458f6c62
Yao0420 <541950428@qq.com>
2019-11-17 22:12:18 +0800

    域名

--- YTeaShop-SpringBoot-PC/config/dev.env.js
+++ YTeaShop-SpringBoot-PC/config/dev.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://localhost:8080/admin"'
+  BASE_API:'"https://tea.5ixp.cn/admin"'
+  // BASE_API: '"http://localhost:8080/admin"'
 }
