fbd0df62bf3dedd6035898d66b5d3b1be7068ceb
Yao0420 <541950428@qq.com>
2019-11-18 21:10:05 +0800

    test

--- YTeaShop-SpringBoot-PC/config/dep.env.js
+++ YTeaShop-SpringBoot-PC/config/dep.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
   ENV_CONFIG: '"dep"',
-  // BASE_API:'"https://tea.5ixp.cn/admin"'
-  BASE_API: '"http://localhost:8080/admin"'
+  BASE_API:'"https://tea.5ixp.cn/admin"'
+  // BASE_API: '"http://localhost:8080/admin"'
 }
